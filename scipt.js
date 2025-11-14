1efa451522ca819911f7661dc40ba38dd071f085
Denny <kleinerumbero@gmail.com>
2025-11-14 15:15:59 +0100

    new js,  dialog added, css fixed

--- scipt.js
+++ scipt.js
@@ -11,47 +11,23 @@ let myImgs = ['antarctica.jpg',
     'surfer.jpg',
     'tree.jpg'
 ];
-let path = './img/'
+
 
 let currentIndex = 0;
+let lightbox = document.getElementById("lightbox");
 
-let lightbox;
 
-
-/*
-
-window.onload = function () {
-    let container = document.getElementById("content");
-
-    // schleife für alle Bilder, die den Namen augibt, 
-    // div und img container erzeugt und sie mit src und class austattet
-    for (let i = 0; i < myImgs.length; i++) {
-
-        let fullPath = path + myImgs[i];
-
-        let divContainer = document.createElement("div");
-        divContainer.className = "img-container";
-
-        let imgElement = document.createElement("img");
-        imgElement.src = fullPath;
-
-        divContainer.appendChild(imgElement);
-        container.appendChild(divContainer);
-    }
-};
-*/
 
 
 window.onload = function () {
 
-    lightbox = this.document.getElementById ("lightbox");
+    
     generateImgs();
     lightboxPicker();
-    initButtons();
-    initCloseButton();
-    initKeyboard();
-    showCurrentImg();
-    closeLightbox();
+    closeBtn ();
+   
+
+
 
 };
 
@@ -63,20 +39,21 @@ function generateImgs() {
     // div und img container erzeugt und sie mit src und class austattet
 
     for (let i = 0; i < myImgs.length; i++) {
-        let fullPath = path + myImgs[i];
-        container.innerHTML += `<div class = "img_container">
-                                    <img src = ${fullPath}>
-                                    
-                                </div>`
+
+        container.innerHTML += `<figure class = "img_container">
+                                    <button class= "column">
+                                        <img src ="./img/${myImgs[i]}">
+                                    </button>
+                                </figure>`
 
     };
 };
 
 // macht die lightbox sichtbar mit einer zusätzlichen Klasse, lädt das Bild ind die lightbox
 // current Index ist ganz oben definiert mit 0
-function lightboxPicker() {
+function lightboxPicker() { // überarbeiten
 
-    
+
     let content = document.getElementById("content");
     let showedImgs = content.getElementsByTagName("img");
     let lightboxImg = document.getElementById("img-lightbox");
@@ -85,18 +62,23 @@ function lightboxPicker() {
 
         showedImgs[i].addEventListener("click", function () {
 
-            lightbox.classList.add('active');
+            lightbox.showModal();
+            lightbox.focus();
 
             lightboxImg.src = showedImgs[i].src;
 
             currentIndex = i;
 
             showImageName(i);
+            showCurrentImg();
 
         });
     };
 
 };
+
+
+
 // seperate Funktion für einen "schönen" Namen, um lightboxPicker() übersichtlich zu halten
 function showImageName(index) {
 
@@ -106,77 +88,65 @@ function showImageName(index) {
     imgDescription.innerHTML = imgName.charAt(0).toUpperCase() + imgName.slice(1);
 
 };
-// Funktion für Buttons links und rechts 
-function initButtons() {
-
-    let buttonRight = document.getElementById("button-right");
-    let buttonLeft = document.getElementById("button-left");
-
-    buttonRight.addEventListener("click", function () {
-        currentIndex++;
-
-        showCurrentImg();
-
-    });
-
-    buttonLeft.addEventListener("click", function () {
-        currentIndex--;
-
-        showCurrentImg();
-    });
-
-};
-// Funktion Closebutton
-function initCloseButton() {
+function closeBtn () {
+const closeBtn = document.getElementById("closeButtonLightbox");
+closeBtn.addEventListener("click", function () {
     
-    let closeBtn = document.getElementById("closeButtonLightbox");
+        lightbox.close();
+    
+});
+};
 
-    closeBtn.addEventListener("click", function () {
-        lightbox.classList.remove("active");
-    });
-};
-// Funktion für Pfeiltasten
-function initKeyboard() {
-    document.addEventListener('keydown', function (event) {
-        if (lightbox.classList.contains("active")) {
-            if (event.key === "ArrowLeft") {
-                currentIndex--;
-                showCurrentImg();
-            };
-            if (event.key === "ArrowRight") {
-                currentIndex++;
-                showCurrentImg();
-            };
-        };
-    });
-};
+
 // Funktion für das aktualisieren des Imgs in der Lightbox
 function showCurrentImg() {
+
     let counter = document.getElementById("img-counter");
     let lightboxImg = document.getElementById("img-lightbox");
 
+
+
     if (currentIndex >= myImgs.length) {
         currentIndex = 0;
+        console.log(currentIndex);
     };
     if (currentIndex < 0) {
         currentIndex = myImgs.length - 1;
+        console.log(currentIndex);
     };
 
-    lightboxImg.src = path + myImgs[currentIndex];
+    lightboxImg.src = "./img/" + myImgs[currentIndex];
 
     showImageName(currentIndex);
 
     counter.innerHTML = (currentIndex + 1) + " / " + myImgs.length;
 };
 
-// Funtion für das Schließen der Lightbox mit prüfen, ob das geklickte Element die Lightbox ist
-function closeLightbox() {
-    
-    lightbox.addEventListener("click", function (event) {
-        if (event.target === lightbox) {
-            lightbox.classList.remove("active");
-        };
-    });
+// Funktion für Pfeiltasten
 
+lightbox.addEventListener("keydown", function (event){
+    if (event.key === "ArrowRight") {
+        next();
+    }
+    if (event.key === "ArrowLeft") {
+        previous();
+    }
+});
+
+
+function next() {
+    currentIndex++;
+    if (currentIndex === myImgs.length - 1) {
+        currentIndex = 0
+    }
+    showCurrentImg();
+};
+
+function previous() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = myImgs.length - 1
+    }
+    showCurrentImg();
 };
 
